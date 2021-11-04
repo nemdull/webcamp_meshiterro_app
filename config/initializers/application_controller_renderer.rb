6b2f64bf34846bca9300ce54f480b2e379e0d346
nemdull <dlanx@outlook.jp>
2021-11-04 05:07:29 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f5a8972c1d768253d82bffc5449b006bc574044e5604e08c53876de69aa0bb78ca62f9fb8320d318d08f98e4a5209c4b2b8bcfb0d4d42b0a70d3f5387cbbe716'
